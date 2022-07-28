77e31ca74ca53db3a50b4d74a502bf34fcccf104
joydip mondal <joydip.mondal@gmail.com>
2022-07-28 15:25:08 +0530

    Assignment-1 created

---
+++ Codes/FirstApp/src/Calculation.java
@@ -0,0 +1,7 @@
+
+public class Calculation {
+	public int addNumbers(int first, int second) {
+		int result = first+second;
+		return result; 
+	}
+}
---
+++ Codes/FirstApp/src/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+
+	public static void main(String[] args) {
+		Calculation calcObjAddress = new Calculation();
+		int output = calcObjAddress.addNumbers(10, 20);
+		System.out.println(output);
+	}	
+}
