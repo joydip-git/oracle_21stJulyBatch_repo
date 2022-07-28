c2a0d64e040def89ec8b14c9bb0465665449c5eb
joydip mondal <joydip.mondal@gmail.com>
2022-07-28 16:32:58 +0530

    Calculator app updated

--- Codes/FirstApp/src/Program.java
+++ Codes/FirstApp/src/Program.java
@@ -2,6 +2,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		Calculation calcObjAddress = new Calculation();
+		
+		//accept two numbers from console 
+		//pass those two numbers to addNumbers method
 		int output = calcObjAddress.addNumbers(10, 20);
 		System.out.println(output);
 	}	
