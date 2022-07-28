d862aac453f962e41430f3bcd6365223a1c0b395
joydip mondal <joydip.mondal@gmail.com>
2022-07-28 17:05:48 +0530

    calculator app updated

--- Codes/FirstApp/src/Program.java
+++ Codes/FirstApp/src/Program.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Program {
 
-	public static void main(String[] args) {
-		Calculation calcObjAddress = new Calculation();
+	public static void main(String[] args) {	
 		
-		//accept two numbers from console 
+		//accept two numbers from console
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("enter first number: ");
+		int firstNumber = scanner.nextInt();
+		
+		System.out.print("enter second number: ");
+		int secondNumber = scanner.nextInt();		
+		
+		scanner.close();
+		
 		//pass those two numbers to addNumbers method
-		int output = calcObjAddress.addNumbers(10, 20);
-		System.out.println(output);
+		Calculation calcObjAddress = new Calculation();
+		int output = calcObjAddress.addNumbers(firstNumber, secondNumber);
+		System.out.println("Result: "+output);
+		
+		
 	}	
 }
