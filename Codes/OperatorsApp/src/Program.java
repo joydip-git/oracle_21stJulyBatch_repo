851708db7b1e7fa06a0ffc269b0a860e3e2933a4
joydip mondal <joydip.mondal@gmail.com>
2022-07-29 11:18:07 +0530

    operators (contd.)

---
+++ Codes/OperatorsApp/src/Program.java
@@ -0,0 +1,13 @@
+
+public class Program {
+	public static void main(String[] args) {
+		int a = 10;
+		int b=0;
+		b = ++a; //a= a+1;
+		System.out.println(b);
+		
+		int c =20;	
+		c+= a;//c=c+a
+		System.out.println(c);
+	}
+}
