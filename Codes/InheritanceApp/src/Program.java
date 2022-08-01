51020a2a9014907bf9b3448cc656fff9c735d215
joydip mondal <joydip.mondal@gmail.com>
2022-08-01 16:02:32 +0530

    inheritance app uploaded

---
+++ Codes/InheritanceApp/src/Program.java
@@ -0,0 +1,8 @@
+
+public class Program {
+	public static void main(String[] args) {
+		String subjects[] = new String[] { "Java", "JavaScript" };
+		Trainer joydipTrainerRef = new Trainer("Joydip", "Mondal", "Bangalore", 10000, "Web", subjects);
+		Trainee akshatTraineeRef = new Trainee("Akshat", "Ved", "Mumbai", 20000, "Dev", "CITA", "Oracle");
+	}
+}
---
+++ Codes/InheritanceApp/src/Trainer.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+
+/**
+ * @author satarupa
+ *
+ */
+public class Trainer extends Person {
+
+	private String domain;
+	private String subjects[];
+
+	/**
+	 * Default constructor
+	 */
+	public Trainer() {
+		// TODO Auto-generated constructor stub
+		System.out.println("[Trainer] default ctor");
+	}
+	
+	public Trainer(String firstName, String lastName, String address, float salary, String domain, String subjects[]) {
+		super(firstName, lastName, address, salary);		
+		System.out.println("[Trainer] parameterized ctor");	
+		this.domain = domain;
+		this.subjects = subjects;
+				
+	}
+
+	/**
+	 * @return the domain
+	 */
+	public String getDomain() {
+		return domain;
+	}
+
+	/**
+	 * @param domain the domain to set
+	 */
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+	/**
+	 * @return the subjects
+	 */
+	public String[] getSubjects() {
+		return subjects;
+	}
+
+	/**
+	 * @param subjects the subjects to set
+	 */
+	public void setSubjects(String[] subjects) {
+		this.subjects = subjects;
+	}
+
+}
